e2051230bcb41a525af24a5cc6a4e81d5a9f3da0
wandji <wbertrand10@gmail.com>
2021-04-17 19:25:21 +0100

    Use dom to create section for tasklist items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 import './styles.scss';
+ 
+let taskList = [];
+
+function start(){
+  clearContent();
+  displayList()
+}
+
+
+function clearContent(){
+  const content = document.querySelector('#content');
+  content.innerHTML = '';
+}
+
+
+
+
+function displayList(){
+  const content = document.getElementById('content');
+  const listContainer = content.appendChild(document.createElement('div'));
+  listContainer.setAttribute('class','list-container mb-4')
+  const heading = listContainer.appendChild(document.createElement('h3'));
+  heading.innerHTML = 'My Task List'
+  const taskList = listContainer.appendChild(document.createElement('ul'));
+  taskList.setAttribute('class', 'list-container mb-4');
+
+  // let list =  localStorage.getItem('taskList') || [];
+  let list = [
+          {
+            name: 'School',
+            id: 1,
+            tasks:[]
+          },
+          {
+            name: 'Home',
+            id: 1,
+            tasks:[]
+          },
+          {
+            name: 'Personal',
+            id: 1,
+            tasks:[]
+          }
+        ];
+  list.forEach((item)=>{
+    const listItem = taskList.appendChild(document.createElement('li'));
+    listItem.textContent = item.name;
+    listItem.setAttribute('class', item.id);
+  })
+}
+
+displayList();
