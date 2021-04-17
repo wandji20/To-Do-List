d1b4d9939ba1c2145a911f84e0893dc481ede7b0
wandji <wbertrand10@gmail.com>
2021-04-17 23:48:07 +0100

    Add list class and event listener to add new list

--- src/index.js
+++ src/index.js
@@ -1,53 +1,100 @@
 import './styles.scss';
- 
-let taskList = [];
+
+let selectedListId = localStorage.getItem('selectedListId');
+
+
+
+
+function getList(){
+  if(localStorage.getItem('taskLists')){
+    return JSON.parse(localStorage.getItem('taskLists'))
+  }else{
+    return [];
+  }
+}
+
+class List {
+  constructor(name){
+    this.name = name;
+    this.id = Date.now().toString();
+  }
+  getName(){
+    return this.name
+  }
+};
+
+function clearContent(item){
+  item.textContent = '';
+}
 
 function start(){
-  clearContent();
-  displayList()
-}
-
-
-function clearContent(){
-  const content = document.querySelector('#content');
-  content.innerHTML = '';
-}
-
-
-
-
-function displayList(){
   const content = document.getElementById('content');
   const listContainer = content.appendChild(document.createElement('div'));
   listContainer.setAttribute('class','list-container mb-4')
   const heading = listContainer.appendChild(document.createElement('h3'));
   heading.innerHTML = 'My Task List'
-  const taskList = listContainer.appendChild(document.createElement('ul'));
-  taskList.setAttribute('class', 'list-container mb-4');
+  const listUl = listContainer.appendChild(document.createElement('ul'));
+  listUl.setAttribute('class', 'list');
 
-  // let list =  localStorage.getItem('taskList') || [];
-  let list = [
-          {
-            name: 'School',
-            id: 1,
-            tasks:[]
-          },
-          {
-            name: 'Home',
-            id: 1,
-            tasks:[]
-          },
-          {
-            name: 'Personal',
-            id: 1,
-            tasks:[]
-          }
-        ];
-  list.forEach((item)=>{
-    const listItem = taskList.appendChild(document.createElement('li'));
-    listItem.textContent = item.name;
-    listItem.setAttribute('class', item.id);
+
+  const formContainer = listContainer.appendChild(document.createElement('div'));
+  const listForm = formContainer.appendChild(document.createElement('form'));
+  const listInput = listForm.appendChild(document.createElement('input'));
+  listInput.setAttribute('type', 'text');
+  listInput.setAttribute('placeholder', '   Add a task list');
+  listInput.setAttribute('class', 'list-name py-1')
+  let listBtn = listForm.appendChild(document.createElement('span'));
+  listBtn.setAttribute('class','btn px-2 py-1 text-light fs-1 list-btn');
+  listBtn.textContent = '+'
+
+}
+start()
+
+
+
+function displayList(){
+  const listUl = document.querySelector('.list')
+  clearContent(listUl);
+
+  const taskLists = getList();
+  console.log(taskLists)
+
+  taskLists.forEach((list)=>{
+    const listItem = listUl.appendChild(document.createElement('li'));
+    listItem.textContent = list.name;
+    listItem.setAttribute('class', list.id);
+    // if(selectedListId === list.id){
+    //   listItem.classList.add('active')
+    // }
   })
+
+}
+displayList()
+
+
+function createList(){
+  let list = getList();
+  const listName = document.querySelector('.list-name');
+  if (listName.value !== ''){
+    const newList = new List(listName.value);
+    list.push(newList);
+    localStorage.taskLists = JSON.stringify(list);
+  }
+  displayList()
 }
 
-displayList();
+const listBtn = document.querySelector('.list-btn')
+listBtn.addEventListener('click', createList)
+
+// const listUl = document.querySelector('.list')
+// listUl.addEventListener('click', (e)=>{
+  
+//   const item = e.target.tagName.toLowerCase();
+//   if (item === 'li'){
+//     displayListTasks()
+//   }
+// })
+
+
+
+
